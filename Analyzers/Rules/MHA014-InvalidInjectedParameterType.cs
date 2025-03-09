a31c382199a5667a04a669ecf3908618d6cfd46a
Aaron Maslen <aaron.maslen@gmail.com>
2025-03-09 14:06:25 +1100

    Report MHA014 at parameter type node

--- Analyzers/Rules/MHA014-InvalidInjectedParameterType.cs
+++ Analyzers/Rules/MHA014-InvalidInjectedParameterType.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
 
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace MicroUtils.HarmonyAnalyzers.Rules;
 
@@ -27,7 +29,7 @@ internal readonly struct InvalidInjectedParameterType : IPatchMethodRule
     public ImmutableArray<Diagnostic> Check(
         PatchMethodData methodData,
         SemanticModel _1,
-        CancellationToken _2)
+        CancellationToken ct)
     {
         if (methodData.PatchType is HarmonyConstants.HarmonyPatchType.Transpiler or HarmonyConstants.HarmonyPatchType.ReversePatch)
             return [];
@@ -35,7 +37,21 @@ internal readonly struct InvalidInjectedParameterType : IPatchMethodRule
         return methodData.PatchMethod.Parameters
             .Select(p => (param: p, expected: HarmonyHelpers.GetInjectionParameterType(p.Name, methodData)))
             .Where(p => p.expected is { } s && !methodData.Compilation.ClassifyConversion(s, p.param.Type).IsStandardImplicit())
-            .SelectMany(p => methodData.CreateDiagnostics(Descriptor, p.param.Locations, messageArgs: [p.param.Type, p.param.Name, p.expected]))
+            .SelectMany(p =>
+            {
+                var locations =
+                    
+                    p.param.DeclaringSyntaxReferences
+                        .Select(sr => sr.GetSyntax(ct))
+                        .OfType<ParameterSyntax>()
+                        .Select(p => p.Type?.GetLocation() ?? p.GetLocation())
+                        .ToImmutableArray();
+
+                if (locations.IsDefaultOrEmpty)
+                    locations = p.param.Locations;
+
+                return methodData.CreateDiagnostics(Descriptor, locations, messageArgs: [p.param.Type, p.param.Name, p.expected]);
+            })
             .ToImmutableArray();
     }
 }
