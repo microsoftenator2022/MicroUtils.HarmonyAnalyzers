d73ddc41006044c7ac46daf26c9d798f5cf27a88
Aaron Maslen <aaron.maslen@gmail.com>
2025-03-10 19:10:07 +1100

    Test for visibility of non-public external metadata

---
+++ Test/NonPublicExternalMethodTest.cs
@@ -0,0 +1,23 @@
+﻿namespace MicroUtils.HarmonyAnalyzers.Test;
+
+using Verify = PatchClassAnalyzerVerifier;
+
+[TestClass]
+public class NonPublicExternalMethodTest
+{
+    [TestMethod]
+    public async Task CanSeeNonPublicExternalMetadata()
+    {
+        const string testPatch = """
+using HarmonyLib;
+
+[HarmonyPatch(typeof(Traverse), "Resolve")]
+static class Patch
+{
+    static void Postfix() {}
+}
+""";
+
+        await Verify.VerifyAnalyzerAsync(testPatch, []);
+    }
+}
