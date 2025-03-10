30c19b7ffe64cace5a58fb7b852ae53245a29ccb
Aaron Maslen <aaron.maslen@gmail.com>
2025-03-10 12:26:22 +1100

    Use `ref` keyword for MHA016 location

--- Analyzers/Rules/MHA016-UseOutForPrefixStateInjection.cs
+++ Analyzers/Rules/MHA016-UseOutForPrefixStateInjection.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading;
 
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace MicroUtils.HarmonyAnalyzers.Rules;
 
@@ -26,17 +27,30 @@ internal readonly struct UseOutForPrefixStateInjection : IPatchMethodRule
     public ImmutableArray<Diagnostic> Check(
         PatchMethodData methodData,
         SemanticModel _1,
-        CancellationToken _2)
+        CancellationToken ct)
     {
         if (methodData.PatchType is not HarmonyConstants.HarmonyPatchType.Prefix)
             return [];
 
         if (methodData.PatchMethod.Parameters.FirstOrDefault(p => p.Name == "__state") is { } stateParam &&
-            stateParam.RefKind is not RefKind.Out or RefKind.Ref)
+            stateParam.RefKind is not RefKind.Out)
         {
             var severity = stateParam.RefKind is RefKind.Ref ? DiagnosticSeverity.Info : DiagnosticSeverity.Warning;
+            
+            ImmutableArray<Location> locations = default;
 
-            return methodData.CreateDiagnostics(Descriptor, primaryLocations: stateParam.Locations, severity: severity);
+            if (stateParam.RefKind is RefKind.Ref)
+                locations = stateParam.DeclaringSyntaxReferences
+                    .Select(sr => sr.GetSyntax(ct))
+                    .OfType<ParameterSyntax>()
+                    .Choose(n => n.Modifiers.TryFirst(m => m.IsKind(Microsoft.CodeAnalysis.CSharp.SyntaxKind.RefKeyword)))
+                    .Select(n => n.GetLocation())
+                    .ToImmutableArray();
+
+            if (locations.IsDefaultOrEmpty)
+                locations = stateParam.Locations;
+
+            return methodData.CreateDiagnostics(Descriptor, primaryLocations: locations, severity: severity);
         }
 
         return [];
