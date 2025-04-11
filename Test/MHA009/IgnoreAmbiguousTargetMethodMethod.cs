3cfe9d6af701ea9d72ac69f2c1e08e50a53687b9
Aaron Maslen <aaron.maslen@gmail.com>
2025-04-11 14:46:18 +1000

    Don't emit MH009 when method is ambiguous between TargetMethod and TargetMethods

---
+++ Test/MHA009/IgnoreAmbiguousTargetMethodMethod.cs
@@ -0,0 +1,37 @@
+﻿namespace MicroUtils.HarmonyAnalyzers.Test.MHA009;
+
+using Verify = PatchClassAnalyzerVerifier;
+
+[TestClass]
+public class IgnoreAmbiguousTargetMethodMethod
+{
+    const string targetClass = """
+class TargetType
+{
+    public void TargetMethod() {}
+    public string TargetProperty => "";
+}
+""";
+
+    [TestMethod]
+    public async Task DontReportIfPatchTypeConflict()
+    {
+        const string testPatch = """
+using System.Collections.Generic;
+using System.Reflection;
+
+using HarmonyLib;
+
+[{|#0:HarmonyPatch|}]
+static class {|#1:Patch|}
+{
+    [{|#2:HarmonyTargetMethods|}]
+    static IEnumerable<MethodInfo> {|#3:TargetMethod|}() => System.Array.Empty<MethodInfo>();
+
+    static void Postfix() {}
+}
+""";
+
+        await Verify.VerifyAnalyzerAsync(new TestSources(targetClass, testPatch), [], disabledDiagnostics: ["MHA007"]);
+    }
+}
